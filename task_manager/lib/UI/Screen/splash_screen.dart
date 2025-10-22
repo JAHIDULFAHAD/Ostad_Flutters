0d7718a2657bfda5c5da00cebd45f5cadff5dc53
Jahidul Haque Fahad <jahidulh829@gmail.com>
2025-10-22 17:18:19 +0600

    Chnage Logo

--- task_manager/lib/UI/Screen/splash_screen.dart
+++ task_manager/lib/UI/Screen/splash_screen.dart
@@ -43,7 +43,7 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Center(
           child: SvgPicture.asset(
             AssetPaths.logo,
-            height: 40,
+            height: 150,
           ),
         ),
       )
