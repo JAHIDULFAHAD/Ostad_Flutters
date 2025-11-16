00f81909c37c92848fc629aa4b7217a8bf36206d
jahid <jahidulh829@gmail.com>
2025-11-17 02:46:05 +0600

    Student data add and fetc done

---
+++ firebase_live_score_app/android/app/src/main/kotlin/jhf/example/firebase_live_score_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package jhf.example.firebase_live_score_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ firebase_live_score_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_live_score_app/app.dart';
+import 'package:firebase_live_score_app/fcm_service.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FcmService.intialize();
+  print(await FcmService.getToken());
+
+  //Flutter error
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  FirebaseAnalytics.instance.setUserId(id: '1234');
+  FirebaseCrashlytics.instance.setCustomKey('User_id', '1234');
+  runApp(const FootballLiveScoreApp());
+}
+
+
